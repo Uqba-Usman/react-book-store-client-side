c3dab57105a6bb189ced8bee9de370c8aa21d9ba
Uqba-Usman <57703614+Uqba-Usman@users.noreply.github.com>
2020-08-19 18:21:20 +0500

    baseurl change

--- src/services/GenericService.js
+++ src/services/GenericService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:4500/api/";
+axios.defaults.baseURL = "https://uuttapp.herokuapp.com/api/";
+// axios.defaults.baseURL = "http://localhost:4500/api/";
 axios.defaults.headers.common["x-auth-token"] = localStorage.getItem("token");
 // const myURL = "http://localhost:4000/api/";
 class GenericService {
